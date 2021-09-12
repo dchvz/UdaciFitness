683973205236c422b22644cc53be74a3ea24a2d7
Diego Alejandro Chávez Soria <diegochavezsoria@gmail.com>
2021-09-12 15:51:19 -0400

    feat: add basic styling

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import  { View, TouchableOpacity, Text } from 'react-native'
+import  { View, TouchableOpacity, Text, StyleSheet, Platform } from 'react-native'
 import { getMetricMetaInfo, timeToString, getDailyReminder  } from '../utils/helpers'
 import { submitEntry, deleteEntry } from '../utils/api'
 import UdaciSlider from './UdaciSlider'
@@ -9,16 +9,19 @@ import { Ionicons } from '@expo/vector-icons'
 import TextButton from './TextButton'
 import { connect } from 'react-redux'
 import { addEntry } from '../actions'
+import { purple, white } from '../utils/colors'
 
 function SubmitBtn ({onPress}) {
   return (
     <TouchableOpacity
+      style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.androidSubmitBtn}
       onPress={onPress}
     >
-      <Text>Submit</Text>
+      <Text style={styles.submitBtnText}>Submit</Text>
     </TouchableOpacity>
   )
 }
+// check the button styling again
 
 class AddEntry extends Component {
   state = {
@@ -97,27 +100,27 @@ class AddEntry extends Component {
     const metaInfo = getMetricMetaInfo()
     if (this.props.alreadyLogged) {
       return (
-        <View>
+        <View style={styles.center}>
           <Ionicons
-            name='ios-happy-outline'
+            name={Platform.OS === 'ios' ? 'ios-happy-outline': 'md-happy'}
             size={100}
           />
           <Text>You already logged your information for today</Text>
-          <TextButton onPress={this.reset}>
+          <TextButton onPress={this.reset} style={{padding: 10}}>
             Reset
           </TextButton>
         </View>
       )
     }
     return (
-      <View>
+      <View style={styles.container}>
         <DateHeader date={(new Date()).toLocaleDateString()} />
         {
           Object.keys(metaInfo).map((key) => {
             const { getIcon, type, ...rest} = metaInfo[key]
             const value = this.state[key]
             return (
-              <View key = { key }>
+              <View key = { key } style={styles.row}>
                 { getIcon() }
                 { type === 'slider'
                   ? <UdaciSlider
@@ -142,6 +145,49 @@ class AddEntry extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center'
+  },
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: white
+  },
+  iosSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40
+  },
+  androidSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    height: 45,
+    borderRadius: 2,
+    alignSelf: 'flex-end',
+    justifyContent: 'center'
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: 'center'
+  },
+  center: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 30,
+    marginRight: 30
+  }
+})
 
 function mapStateToProps (state) {
   const key = timeToString()
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import {View} from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
+import { white, red, orange, blue, lightPurp, pink } from './colors'
 
 export function isBetween (num, x, y) {
   if (num >= x && num <= y) {
@@ -37,6 +38,18 @@ export function calculateDirection (heading) {
   return direction
 }
 
+const styles = StyleSheet.create({
+  iconContainer: {
+    padding: 5,
+    borderRadius: 8,
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 20
+  }
+})
+
 export function timeToString (time = Date.now()) {
   const date = new Date(time)
   const todayUTC = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()))
@@ -53,10 +66,10 @@ export function getMetricMetaInfo (metric) {
       type: 'steppers',
       getIcon() {
         return (
-          <View>
+          <View style={[styles.iconContainer, {backgroundColor: red}]}>
             <MaterialIcons
               name = 'directions-run'
-              color = {'black'}
+              color = {white}
               size = {35}
             />
           </View>
@@ -71,10 +84,10 @@ export function getMetricMetaInfo (metric) {
       type: 'steppers',
       getIcon() {
         return (
-          <View>
+          <View style={[styles.iconContainer, {backgroundColor: orange}]}>
             <MaterialCommunityIcons
               name = 'bike'
-              color = {'black'}
+              color = {white}
               size = {35}
             />
           </View>
@@ -89,10 +102,10 @@ export function getMetricMetaInfo (metric) {
       type: 'steppers',
       getIcon() {
         return (
-          <View>
+          <View style={[styles.iconContainer, {backgroundColor: blue}]}>
             <MaterialCommunityIcons
               name = 'swim'
-              color = {'black'}
+              color = {white}
               size = {35}
             />
           </View>
@@ -107,10 +120,10 @@ export function getMetricMetaInfo (metric) {
       type: 'slider',
       getIcon() {
         return (
-          <View>
+          <View style={[styles.iconContainer, {backgroundColor: lightPurp}]}>
             <FontAwesome
               name = 'bed'
-              color = {'black'}
+              color = {white}
               size = {35}
             />
           </View>
@@ -125,10 +138,10 @@ export function getMetricMetaInfo (metric) {
       type: 'slider',
       getIcon() {
         return (
-          <View>
+          <View style={[styles.iconContainer, {backgroundColor: pink}]}>
             <MaterialCommunityIcons
               name = 'food'
-              color = {'black'}
+              color = {white}
               size = {35}
             />
           </View>
